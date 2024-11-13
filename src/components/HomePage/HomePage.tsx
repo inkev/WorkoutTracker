5e28bc0fb1dc1663c9b3fc9423ba61531ab076c7
inkev <kevin380@live.com>
2024-11-12 20:05:27 -0800

    Added table to landing page

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -2,7 +2,20 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import './Calendar.css'
 import 'react-calendar/dist/Calendar.css';
+import cx from 'clsx';
+import { Table, ScrollArea } from '@mantine/core';
+import classes from './TableScrollArea.module.css';
 
+const data = [{
+    name: 'Athena Weissnat',
+    company: 'Little - Rippin',
+    email: 'Elouise.Prohaska@yahoo.com',
+  },
+  {
+    name: 'Deangelo Runolfsson',
+    company: 'Greenfelder - Krajcik',
+    email: 'Kadin_Trantow87@yahoo.com',
+  }]
 
 const HomePage = () => {
     type ValuePiece = Date | null;
@@ -10,14 +23,40 @@ const HomePage = () => {
 
     const [value, onChange] = useState<Value>(new Date());
 
+    const [scrolled, setScrolled] = useState(false)
+
+    const rows = data.map((row) => (
+        <Table.Tr key={row.name}>
+          <Table.Td>{row.name}</Table.Td>
+          <Table.Td>{row.email}</Table.Td>
+          <Table.Td>{row.company}</Table.Td>
+        </Table.Tr>
+      ));
+
     return (
-        <div className="Sample">
-          <div className="Sample__container">
-            <main className="Sample__container__content">
-              <Calendar onChange={onChange} value={value} />
-            </main>
-          </div>
+        <div>
+            <div className="Sample">
+                <div className="Sample__container">
+                    <main className="Sample__container__content">
+                    <Calendar onChange={onChange} value={value} />
+                    </main>
+                </div>
+            </div>
+        <ScrollArea h={300} onScrollPositionChange={({ y }) => setScrolled(y !== 0)}>
+         <Table miw={700}>
+           <Table.Thead className={cx(classes.header, { [classes.scrolled]: scrolled })}>
+             <Table.Tr>
+               <Table.Th>Name</Table.Th>
+               <Table.Th>Email</Table.Th>
+               <Table.Th>Company</Table.Th>
+             </Table.Tr>
+           </Table.Thead>
+           <Table.Tbody>{rows}</Table.Tbody>
+         </Table>
+       </ScrollArea>
         </div>
+
+
       );
 }
 
